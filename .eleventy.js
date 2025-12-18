dbeb9b59d555f2fda9c9af234e8717916381bb54
Denni Rauschenberg <d.rauschenberg98@gmail.com>
2025-12-18 23:57:18 +0100

    Improvement Layout and UI + projects page added

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
-  
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
-    };
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addGlobalData("buildYear", new Date().getFullYear());
+  eleventyConfig.addCollection("projekte", function (collectionApi) {
+  eleventyConfig.addGlobalData("site", {
+  name: "Denni Rauschenberg",
+  domain: "denni-rauschenberg.de"
+});
+
+    return collectionApi.getFilteredByTag("projekte");
+  });
+
+  return {
+    dir: { input: "src", output: "_site" }
   };
-  
+};
