924f818ad4bbc9ac7153ba8eba7c390f46e6b66c
Denni Rauschenberg <d.rauschenberg98@gmail.com>
2025-12-23 16:19:22 +0100

    Update Website: Added Settings: Dark/White theme. New Home screen design

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,25 @@
 module.exports = function (eleventyConfig) {
+  // Static assets nach _site kopieren
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addGlobalData("buildYear", new Date().getFullYear());
-  eleventyConfig.addCollection("projekte", function (collectionApi) {
-  eleventyConfig.addGlobalData("site", {
-  name: "Denni Rauschenberg",
-  domain: "denni-rauschenberg.de"
-});
 
+  // Globale Daten
+  eleventyConfig.addGlobalData("buildYear", new Date().getFullYear());
+  eleventyConfig.addGlobalData("site", {
+    name: "Denni Rauschenberg",
+    domain: "denni-rauschenberg.de",
+  });
+
+  // Collection: Projekte (Tag "projekte")
+  eleventyConfig.addCollection("projekte", function (collectionApi) {
     return collectionApi.getFilteredByTag("projekte");
   });
 
+  // Wichtig: input=src, includes liegt dann bei src/_includes
   return {
-    dir: { input: "src", output: "_site" }
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
   };
 };
